f9005b0efba7f60477d8aa5a66b044d3bdb0fc34
Heberth Deza <heberth2788@gmail.com>
2025-04-16 11:15:25 -0500

    Initial Commit

---
+++ swiftuilearnandtestTests/swiftuilearnandtestTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuilearnandtestTests.swift
+//  swiftuilearnandtestTests
+//
+//  Created by Heberth Deza on 16/04/25.
+//
+
+import Testing
+@testable import swiftuilearnandtest
+
+struct swiftuilearnandtestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
